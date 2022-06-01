db1d47a8a28afdae38fc96aa6d20b6c4c4bd1ddc
Saad Mohammed Alessa <s.alessa@nicsep.gov.sa>
2022-06-01 15:05:18 +0300

    finish station.py and turnstile_value.json

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -9,7 +9,6 @@ from confluent_kafka.avro import AvroProducer,CachedSchemaRegistryClient
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-# logger.propagate = False
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 rootLogger = logging.getLogger()
 consoleHandler = logging.StreamHandler()
